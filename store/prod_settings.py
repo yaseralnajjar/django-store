69c24e1fc9cd76665426a733e8072b5174661ace
YaserAlnajjar <kasseryasser@windowslive.com>
2020-07-11 16:12:52 +0300

    Add default from email env var

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -21,6 +21,7 @@ DATABASES = {
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'  # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
