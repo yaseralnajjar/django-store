25e2fc2efe8b486bacf0269f9ba335e5647126d5
YaserAlnajjar <kasseryasser@windowslive.com>
2020-07-11 15:31:47 +0300

    Add email settings

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -13,3 +13,12 @@ ALLOWED_HOSTS = ['morning-depths-38898.herokuapp.com']
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'  # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
